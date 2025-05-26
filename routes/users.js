a68893ca66c4b644c7fbbdc955ed03805fb8a05b
CosminSebastian2025 <cosminsebastianf@gmail.com>
2025-05-26 19:03:20 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+// Importa il modulo Express, necessario per creare router e gestire le route
 var express = require('express');
+// Crea un nuovo router utilizzando Express
 var router = express.Router();
 
-/* GET users listing. */
+/*
+  Route GET per la lista degli utenti.
+  Questa route risponde a richieste GET sull'endpoint '/users'.
+  Quando viene chiamata, restituisce una semplice stringa di risposta.
+*/
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource'); // Risposta testuale per la route /users
 });
 
+// Esporta il router per poterlo utilizzare in app.js o in altri file
 module.exports = router;
