1ff6df5324fce1c5024766d27f2bdbab4853d4cd
CosminSebastian2025 <cosminsebastianf@gmail.com>
2025-05-26 19:02:57 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
+// Importa il modulo Express, necessario per creare router e gestire le route
 var express = require('express');
+// Crea un nuovo router utilizzando Express
 var router = express.Router();
 
-/* GET home page. */
+/*
+  Route GET per la home page.
+  Se vuoi mostrare la pagina di login come home, decommenta la funzione qui sotto e commenta la successiva.
+  Questa funzione renderizza la vista 'login.hbs' con il titolo 'Express'.
+*/
 /*router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });*/
 
+/*
+  Route GET per la home page.
+  Questa funzione renderizza la vista 'index.hbs' con il titolo 'Express'.
+  Viene utilizzata come home page principale dell'applicazione.
+*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Esporta il router per poterlo utilizzare in app.js o in altri file
 module.exports = router;
